3a6268e695089d0b1442f5806d01f86751a074d2
AnushkaChaudhary1322 <122356104+AnushkaChaudhary1322@users.noreply.github.com>
2024-07-24 02:57:03 +0000

    container config done

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,21 +1,11 @@
 let score1 = "33abc"
-let score2 = null
-let score3 = undefined
 
-console.log("score1: ", typeof score1);
-console.log("score2: ", typeof score2);
-console.log("score3: ", typeof score3);
+// console.log("score1: ", typeof score1);
 // console.log(typeof (score)); // another way to check 
 
-console.log("After conversion:")
-
 let valueInNumber1 = Number(score1)
-let valueInNumber2 = Number(score2)
-let valueInNumber3 = Number(score3)
 
-console.log("Type of score1: ", typeof valueInNumber1 , "| Actual value: ", valueInNumber1);
-console.log("Type of score2: ", typeof valueInNumber2 , "| Actual value: ", valueInNumber2);
-console.log("Type of score3: ", typeof valueInNumber3 , "| Actual value: ", valueInNumber3);
+// console.log("Type of score1: ", typeof valueInNumber1 , "| Actual value: ", valueInNumber1);
 
 // "33" => 33
 // "33abc" => NaN (not a number)
@@ -29,3 +19,37 @@ console.log("Type of score3: ", typeof valueInNumber3 , "| Actual value: ", valu
 // "" => false
 // "abc" => true
 
+// *.*.*.*.*.*.*.*.*.* Operations *.*.*.*.*.*.*.*.*.*.
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**4);
+// console.log(2/3);
+// console.log(2%3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true) // output: 1
+// console.log(+"") // output: 0
+
+// console.log(null > 0); // false
+// console.log(null == 0); // false
+// console.log(null >= 0); // false
+/**
+  the reason is that an equality check == and comparisons > < >= <= work differently
+  Comparisons convert null to a number, treating it as 0.
+  That's why null >= 0 is true but not null > 0.
+ */
+
+// ===
+
+console.log("2" == 2); // true
+console.log("2" === 2); // false
