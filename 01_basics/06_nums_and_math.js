65fa1eec0d06d6d3dd9a1d0845aeaf26bd4a3b01
AnushkaChaudhary1322 <122356104+AnushkaChaudhary1322@users.noreply.github.com>
2024-07-26 04:03:48 +0000

    Basic Numbers and Math done

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,5 +1,5 @@
 const score = 400 // automatically considers it as number 
-console.log(score);
+// console.log(score);
 
 const balance = new Number(100);
 // console.log(balance);
@@ -18,6 +18,19 @@ const hundreds = 1000000
 
 // *.*.*.*.*.*.*.*.*.* Maths *.*.*.*.*.*.*.*.*.*.*.*
 
+// console.log(Math);
+// console.log(Math.abs(-4)); // 4
+// console.log(Math.round(4.6)); // 5
+// console.log(Math.ceil(4.2)); // for nearest upper value // output: 5
+// console.log(Math.floor(4.8)); // for nearest lower value // output: 4
+// console.log(Math.min(4,8,2,11)); 
+// console.log(Math.max(4,8,2,11)); 
 
+console.log(Math.random()); // values always b/w 0-1
+console.log((Math.random()*10) + 1); 
+console.log(Math.floor(Math.random()*10) + 1); 
 
+const min = 10
+const max = 20
 
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
